913ade54da399d7e2051774a59235ee5ba741678
AditelJunior <aitmatov40@gmail.com>
2025-02-18 20:42:33 +0100

    added key listener to send the message on Enter, added function that listens to changes on textarea

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -166,9 +166,14 @@ const Home = () => {
         if(!htmlPart && modelResult.html) {
             htmlPart = modelResult.html
         }
-
-      //   getAdSize(htmlPart)
         return { text: textPart, html: htmlPart  };
+   }
+   
+   function textAreaHandler(e:any) {
+      const textArea = e.target;
+      textArea.style.height = 59+'px';
+      textArea.style.height = textArea.scrollHeight + 'px';
+      setQueryText(textArea.value);
    }
 
    function openMobilePreview () {
@@ -226,10 +231,7 @@ const Home = () => {
                         <path d="M21.44 11.05l-9.19 9.19a6 6 0 01-8.49-8.49l9.19-9.19a4 4 0 015.66 5.66l-9.2 9.19a2 2 0 01-2.83-2.83l8.49-8.48" /></svg>
                      </button>    
                         
-                     <textarea id="user_input" placeholder="Type your request here..." onChange={(e)=> {
-                        e.target.style.height = 59+'px';
-                        e.target.style.height = e.target.scrollHeight + 'px';
-                        setQueryText(e.target.value);}}/>
+                     <textarea id="user_input" placeholder="Type your request here..." onKeyDown={(e:any)=> (e.key === 'Enter' && !e.shiftKey && !e.ctrlKey && !e.altKey && !e.metaKey) ? (e.preventDefault(), aiResponse()) : null } onChange={(e)=> (textAreaHandler(e))} />
                         
                      <button disabled={disableSending} className="button_no_style send_button" onClick={()=> aiResponse()}><span>➤</span></button>
                      <span className="developed_by">Developed by: <a href="https://www.linkedin.com/in/adilet-aitmatov/" target="_blank">Adilet Aitmatov</a></span>
