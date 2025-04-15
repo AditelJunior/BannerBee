091d751b6f583f50c47a01c7871616198a8418f4
AditelJunior <aitmatov40@gmail.com>
2025-04-15 21:56:57 +0200

    fix: update rules for user-provided images and template handling

--- src/gemini.ts
+++ src/gemini.ts
@@ -40,11 +40,11 @@ const template = `<!DOCTYPE html>
 </html>`
 
 const rules ='You have to use links you received in the html;\n' +
-            "Use images provided by user. Dont look for images by yourself. If you need an image placeholder create html shape and add text that describes it. For example you need to show user a moon. You can create a circle and write on top of it 'Moon' so user understands;\n" +
+            "Use images provided by user. Dont look for images in internet. If you need an image placeholder create html shape and add text that describes it. For example you need to show user a moon. You can create a circle and write inside: 'Moon';\n" +
             "If user asked to animate the image he never sent, you can ask him to do it in the file input field; Never ask him to send URL of the file. You need user to put files in the input field;\n " +
-            "Try to avoid colisions in your animations last frame. Animation should last maximum 30secs if user didnt specify it. It also can be 3 loops by 10secs each or 2 loops  by 15secs each. After 30secs animation should freeze\n " +
+            "Try to avoid colisions in your animations last frame. Animation must last maximum 30secs if user didnt specify it. It also can be 3 loops by 10secs each or 2 loops  by 15secs each. After 30secs animation should freeze\n " +
             // 'For images always use img tag dont use images in css;\n' +
-            "If the user sends background image(shortly - bg) always put it behind everything and dont animate it unless user asks to animate its disappearance; \n" +
+            "If the user sends background image(shortly - bg) always put it behind everything and dont animate it unless user asks to animate it; \n" +
             "If the user sends Call to Action (shortly - CTA) after its apearence animation, it should stay on stage, unless user asks to animate its disappearance; \n" +
             "Always write code in the template! \n" +  
             "Never write code outside of the template! \n" +  
@@ -54,17 +54,16 @@ const rules ='You have to use links you received in the html;\n' +
             "Replace this text '&&WIDTH_OF_THE_BANNER&&' with width of the banner user requesting; \n" + 
             "Replace this text '&&HEIGHT_OF_THE_BANNER&&' with height of the banner user requesting; \n" +
             "Default size is 600x600; \n" +
+            "If user is sending you his own template use it instead of your defaul template and add these metatags if missing and replace placeholders by sizes provided by user: <meta name='viewport' content='width=&&WIDTH_OF_THE_BANNER&&, initial-scale=1.0'> <meta name='ad.size' content='width=&&WIDTH_OF_THE_BANNER&&,height=&&HEIGHT_OF_THE_BANNER&&'>" +
             "Do not add any other <a> tags; If user asks to create a cta do not use <a> to wrap it, use div or button instead.\n" +
             "Try to avoid adding images through css, put it in css only if needed. In normal situtation use img tag" +
             // "Whole html must be wrapped by '```html' and in the end: '```' \n" +
-            "If user is sending you a template use it instead of your defaul template" +
             "If you want to send the link add attribute target='_blank to that link, do not apply this rule to clickTag'\n" +
             "If user is giving poor explanation and didnt send you any instructions on CTA you can add CTA to the banner and some marketing slogans apearing\n";
 
 
 export const model = genAI.getGenerativeModel({ 
     model: "gemini-2.0-flash",
-    systemInstruction: "Your Name is BannerBee, you are html banner generator. Your tools are: HTML, CSS, JS and GSAP. You will receive instructions on how to animate and what to animate, if not make something creative. You will receive urls to images with their descriptions and properties. You must return json object with 2 properties text and html in text property you have to put your text response and in html you have to put created html banner(html may be empty if user didnt ask to generate or fix generated banner it) { 'text': 'string', 'html': 'string' } Rules:\n"+ rules + "\n" +
-    " Here is a template you are going to use: " + template+ ";\n" 
-    // `You can use one of these buttons in the banner: 1 button, with 3D flip effect. with second text on other side. html: <button class="custom-btn btn-12"><span>Click!</span><span>Read More</span></button>; css: .custom-btn{width:130px;height:40px;color:#fff;border-radius:5px;padding:10px25px;font-family:'Lato',sans-serif;font-weight:500;background:transparent;cursor:pointer;transition:all0.3sease;position:relative;display:inline-block;box-shadow:inset2px2px2px0pxrgba(255,255,255,.5),7px7px20px0pxrgba(0,0,0,.1),4px4px5px0pxrgba(0,0,0,.1);outline:none;} .btn-12{position:relative;right:20px;bottom:20px;border:none;box-shadow:none;width:130px;height:40px;line-height:42px;-webkit-perspective:230px;perspective:230px;}.btn-12span{background:rgb(0,172,238);background:linear-gradient(0deg,rgba(0,172,238,1)0%,rgba(2,126,251,1)100%);display:block;position:absolute;width:130px;height:40px;box-shadow:inset2px2px2px0pxrgba(255,255,255,.5),7px7px20px0pxrgba(0,0,0,.1),4px4px5px0pxrgba(0,0,0,.1);border-radius:5px;margin:0;text-align:center;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box;-webkit-transition:all.3s;transition:all.3s;}.btn-12span:nth-child(1){box-shadow:-7px-7px20px0px#fff9,-4px-4px5px0px#fff9,7px7px20px0px#0002,4px4px5px0px#0001;-webkit-transform:rotateX(90deg);-moz-transform:rotateX(90deg);transform:rotateX(90deg);-webkit-transform-origin:50%50%-20px;-moz-transform-origin:50%50%-20px;transform-origin:50%50%-20px;}.btn-12span:nth-child(2){-webkit-transform:rotateX(0deg);-moz-transform:rotateX(0deg);transform:rotateX(0deg);-webkit-transform-origin:50%50%-20px;-moz-transform-origin:50%50%-20px;transform-origin:50%50%-20px;}.btn-12:hoverspan:nth-child(1){box-shadow:inset2px2px2px0pxrgba(255,255,255,.5),7px7px20px0pxrgba(0,0,0,.1),4px4px5px0pxrgba(0,0,0,.1);-webkit-transform:rotateX(0deg);-moz-transform:rotateX(0deg);transform:rotateX(0deg);}.btn-12:hoverspan:nth-child(2){box-shadow:inset2px2px2px0pxrgba(255,255,255,.5),7px7px20px0pxrgba(0,0,0,.1),4px4px5px0pxrgba(0,0,0,.1);color:transparent;-webkit-transform:rotateX(-90deg);-moz-transform:rotateX(-90deg);transform:rotateX(-90deg);}`
+    systemInstruction: "Your Name is BannerBee, you are html banner generator. Your tools are: HTML, CSS, JS and GSAP. You will receive instructions on how to animate and what to animate, if not make something creative. You will receive urls to images with their descriptions and properties. You must return json object with 2 properties text and html in text property you have to put your text response and in html you have to put created html banner(html may be empty if user didnt ask to generate or fix generated banner) { 'text': 'string', 'html': 'string' } Rules:\n"+ rules + "\n" +
+    " Here is a template you are going to use: " + template+ ";\n"
 })
